424fed32f332d8128ef6fbf013b074fa0a38e447
sananabbasov <sanan_abbasov@list.ru>
2022-02-21 14:37:44 +0400

    21.02.2022 14:40

---
+++ K205Oleev/Areas/admin/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using K205Oleev.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace K205Oleev.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class AboutController : Controller
+    {
+        private readonly OleevDbContext _context;
+
+        public AboutController(OleevDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var about = _context.AboutLanguages.Include(x=>x.About).Where(x=>x.LangCode=="Az").ToList();
+            return View(about);
+        }
+    }
+}
