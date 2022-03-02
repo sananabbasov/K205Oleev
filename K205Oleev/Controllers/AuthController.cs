c846a472ba5310131327ba833b1cb5bfd558e409
sananabbasov <sanan_abbasov@list.ru>
2022-03-02 16:37:45 +0400

    02.03.2022 16:40

---
+++ K205Oleev/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+using K205Oleev.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace K205Oleev.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<K205User> _userManager;
+        private readonly SignInManager<K205User> _signInManager;
+        public AuthController(UserManager<K205User> userManager, SignInManager<K205User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            K205User user = new()
+            {
+                UserName = registerVM.Name,
+                Email = registerVM.Email,
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user,registerVM.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+    }
+}
