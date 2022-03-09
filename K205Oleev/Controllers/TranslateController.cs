d8ea9389c39d166e06859fcf049b04020f4ae283
sananabbasov <sanan_abbasov@list.ru>
2022-03-09 14:43:14 +0400

    09.03.2022 14:40

---
+++ K205Oleev/Controllers/TranslateController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K205Oleev.Controllers
+{
+    public class TranslateController : Controller
+    {
+        public IActionResult Index(string lang)
+        {
+            CookieOptions cookieOptions = new();
+            cookieOptions.Path = lang;
+            return View();
+        }
+    }
+}
